9b7f5870e258d7b5f31a7a7b7c72c7c6e923330a
无敌战神 <104905415+WuDi-ZhanShen@users.noreply.github.com>
2022-12-01 21:14:17 +0800

    Update UserService.java

--- app/src/main/java/com/tile/screenoff/UserService.java
+++ app/src/main/java/com/tile/screenoff/UserService.java
@@ -4,10 +4,12 @@ public class UserService extends IUserService.Stub {
 
     @Override
     public void destroy() {
+        System.exit(0);
     }
 
     @Override
     public void exit() {
+        System.exit(0);
     }
 
     @Override
